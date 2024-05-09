e9b472fcd64a8712ca1ff360333065ce11a9d132
pd-smith <pat@Patricks-MacBook-Air.local>
2024-05-08 23:18:33 -0400

    Getting even spicier

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,50 +4,49 @@ import React from 'react';
 // While you're at it, go ahead and clean up that ugly css
 export function Home() {
     return (
-        <>
-            <main>
-                <h1 className="cartoon-border">👋 Hey, I'm Pat</h1>
-                <section className="card cartoon-border">
-                    <h2>💤 Here is my dog</h2>
-                    <img
-                        alt="Miss Ollie the corgi sleeping on the couch, belly up"
-                        src="/ollie.png"
-                    ></img>
-                </section>
-                <section className="card cartoon-border">
-                    <h2>💻 Here are my socials</h2>
-                    <ul>
-                        <li>
-                            <a
-                                target="_blank"
-                                href="https://www.linkedin.com/in/patrsmith/"
-                                rel="noreferrer"
-                            >
-                                LinkedIn
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                target="_blank"
-                                href="https://github.com/pd-smith"
-                                rel="noreferrer"
-                            >
-                                Github
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-            </main>
-            <div className="lettuce-wrap cartoon-border">
-                <hr className="lettuce cartoon-border" />
-                <hr className="lettuce cartoon-border" />
-                <hr className="lettuce cartoon-border" />
-            </div>
-            <button className="exclaim-wrap cartoon-border">
-                <hr className="exclaim cartoon-border" />
-                <hr className="exclaim cartoon-border" />
-                <hr className="exclaim cartoon-border" />
-            </button>
-        </>
+        <main>
+            <h1 className="cartoon-border">
+                👋 <b>Hey, I'm Pat</b>
+            </h1>
+            <section className="card cartoon-border">
+                <h2>
+                    💤 <b>Here is my dog</b>
+                </h2>
+                <img
+                    alt="Miss Ollie the corgi sleeping on the couch, belly up"
+                    src="/ollie.png"
+                ></img>
+                <button className="exclaim-wrap cartoon-border halftone">
+                    <hr className="exclaim cartoon-border" />
+                    <hr className="exclaim cartoon-border" />
+                    <hr className="exclaim cartoon-border" />
+                </button>
+            </section>
+            <section className="card cartoon-border">
+                <h2>
+                    💻 <b>Here are my socials</b>
+                </h2>
+                <ul>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://www.linkedin.com/in/patrsmith/"
+                            rel="noreferrer"
+                        >
+                            LinkedIn
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://github.com/pd-smith"
+                            rel="noreferrer"
+                        >
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </main>
     );
 }
